332693cadf1a96e3ac05350bf591fdaa2de6f352
Dmitry Sheiko <web.sbelarus@gmail.com>
2013-06-25 00:29:23 +0200

    moving build tasks from ANT to Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,9 +3,12 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks("grunt-contrib-jshint");
   grunt.loadNpmTasks("grunt-jscs");
+  grunt.loadNpmTasks("grunt-jsic");
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   
 
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
     jshint: {
       options: {
         jshintrc: ".jshintrc"
@@ -17,10 +20,22 @@ module.exports = function(grunt) {
             "standard": "Jquery"
         },
         all: ["./js/source"]
+    },
+    jsic: {
+        files: ["./js/source/main.js", "./js/build/jquery.html5form.js"]
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: './js/build/jquery.html5form.js',
+        dest: './js/build/jquery.html5form.min.js'
+      }
     }
   });
 
-  grunt.registerTask("test", ["jshint", "jscs"]);
+  grunt.registerTask("test", ["jshint", "jscs", "jsic", "uglify"]);
   grunt.registerTask("default", ["test"]);
 
 };
